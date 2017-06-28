75bb6461e5eaa91c6a9a3a30c07973f81f03e4f4
kachelda <ni@syt.in.ua>
2017-06-29 00:56:04 +0300

    Peredelal

--- lesson2/lesson2/Program.cs
+++ lesson2/lesson2/Program.cs
@@ -13,28 +13,41 @@ namespace lesson2
             int x = 0;
             int y = 0;
             int z = 0;
-            do
+            while ((x != 10) || (y != 15) || (z != 20))
             {
-                if (x != 10)
+                for (int i = 0; i < 100; i++)
                 {
-                    x++;
-                }
-                if (y != 15)
-                {
-                    y++;
-                }
-                if (z != 20)
-                {
-                    z++;
-                }
-                
-            }
-            while (x  < 10|y < 15|z < 20);
-            Console.WriteLine("Число x = {0}", x);
-            Console.WriteLine("Число y = {0}", y);
-            Console.WriteLine("Число z = {0}", z);
-            Console.ReadLine();
+                    if (i == 10)
+                    {
+                        x = i;
+                        break;
+                    }
 
+                }
+                for (int k = 0; k < 100; k++)
+                {
+                    if (k == 15)
+                    {
+                        y = k;
+                        break;
+                    }
+                }
+                for (int m = 0; m < 100; m++)
+                {
+                    if (m == 20)
+                    {
+                        z = m;
+                        break;
+                    }
+
+
+                }
+                Console.WriteLine("Число x = {0}", x);
+                Console.WriteLine("Число y = {0}", y);
+                Console.WriteLine("Число z = {0}", z);
+                Console.ReadLine();
+
+            }
         }
     }
 }
