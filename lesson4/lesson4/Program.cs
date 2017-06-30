14982ea19f83941c402bc00ee29ed47bb8c7de6e
kachelda <ni@syt.in.ua>
2017-06-30 21:30:54 +0300

    yep

---
+++ lesson4/lesson4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[100];                
+                for (int i = 0; i<array.Length; i++)
+            {
+                array[i] = i + 1;
+                Console.WriteLine(array[i]);
+                
+            }
+            Console.Read();
+        }   
+    }
+}
