d444acade34a10fbba194d3db57ca7941bc2bbeb
kachelda <ni@syt.in.ua>
2017-07-02 00:52:42 +0300

    yes

--- lesson5/lesson5/Program.cs
+++ lesson5/lesson5/Program.cs
@@ -12,15 +12,15 @@ namespace lesson5
         {
             List <int> list1 = new List <int> () { };
                        
-            for (int i = 1; i <= 100; i++)
+            for (int i = 0; i < 100; i++)
             {
-                if (i <= 50)
+                if (i < 50)
                 {
-                    list1.Insert(i-1, 51-i);
+                    list1.Insert(i, 50-i);
                 }
                 else
                 {
-                    list1.Insert(i - 1, i);
+                    list1.Insert(i, i+1);
                 }
                        
             }
