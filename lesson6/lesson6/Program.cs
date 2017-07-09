e39a70785961ce803579d75ff416a94f8e56440f
kachelda <ni@syt.in.ua>
2017-07-09 22:37:01 +0300

    end

--- lesson6/lesson6/Program.cs
+++ lesson6/lesson6/Program.cs
@@ -8,6 +8,19 @@ namespace lesson6
 {
     class Program
     {
+        public static int CountContains(List<int> list, int value)
+        {
+            int p = 0;
+            for (int k = 0; k < list.Count; k++)
+            {            
+                if (value == list[k])
+                {
+                    p++;
+                }
+            }
+            return p;
+        }
+
         static void Main(string[] args)
         {
             List<int> list1 = new List<int>() { 0, 0, 0, 5, 5, 5, 9, 9, 9, 0, 5, 9 };
@@ -36,29 +49,15 @@ namespace lesson6
 
             List<int> list2 = new List<int>();
 
-            for (int i = 0; i < list1.Count; i++)
+            list2.Add(list1[0]);
+            for (int i = 1; i < list1.Count; i++)
             {
-                if (list2.Count == 0)
+                if (CountContains(list2, list1[i]) < 2)
                 {
                     list2.Add(list1[i]);
                 }
-                else
-                {
-                    int p = 0;
-                    for (int k = 0; k < list2.Count; k++)
-                    {
-                        if (list1[i] == list2[k])
-                        {
-                            p++;
-                        }
-                    }
-                    if (p < 2)
-                    {
-                        list2.Add(list1[i]);
-                    }
-                
-                }
             }
+                                    
             //вывод нового списка
 
             foreach (int n in list2)
