06fed289127b8eebcb31bdbb5a5e439c880ca45c
kachelda <ni@syt.in.ua>
2017-07-13 22:09:27 +0300

    yep

--- lesson8/lesson8/Program.cs
+++ lesson8/lesson8/Program.cs
@@ -10,34 +10,20 @@ namespace lesson8
     {
         static void Main(string[] args)
         {
-            List<List<int>> list0 = new List<List<int>>() { new List<int>() {3,4}, new List<int>() {1,2}, new List<int>() {5,6} };
+            List<List<int>> list0 = new List<List<int>>() { new List<int>() { 3, 4 }, new List<int>() { 1, 2 }, new List<int>() { 5, 6 } };
             List<int> listfinal = new List<int>();
 
             PrintTwoDimensional(list0);
 
             // формируем новый List
             for (int i = 0; i < list0.Count; i++)
-            { 
+            {
                 for (int j = 0; j < list0[i].Count; j++)
                 {
                     listfinal.Add(list0[i][j]);
+                    Sortirovka(listfinal);
                 }
 
-            }
-
-            //сортировка List
-            int help;
-            for (int i = 0; i < listfinal.Count; i++)
-            {
-                for (int j = 0; j < listfinal.Count; j++)
-                {
-                    if (listfinal[i] < listfinal[j])
-                    {
-                        help = listfinal[i];
-                        listfinal[i] = listfinal[j];
-                        listfinal[j] = help;
-                    }
-                }
             }
 
             Console.WriteLine();
@@ -46,17 +32,8 @@ namespace lesson8
             PrintOneDimensional(listfinal);
             Console.ReadLine();
 
-
         }
-
-        public static void Print(List<int> list)
-        {
-            foreach (int i in list)
-            {
-                Console.Write(" " + i);
-            }
-        }
-
+        
         public static void PrintTwoDimensional(List<List<int>> list)
         {
             Console.Write("{");
@@ -77,5 +54,30 @@ namespace lesson8
             string result = "{" + string.Join(separator, list) + "}";
             Console.Write(result);
         }
+
+        public static void Sortirovka(List<int> list)
+        {
+            int help;
+            for (int i = 0; i<list.Count; i++)
+            {
+                for (int j = 0; j<list.Count; j++)
+                {
+                    if (list[i] < list[j])
+                    {
+                        help = list[i];
+                        list[i] = list[j];
+                        list[j] = help;
+                    }
+                }
+            }
+        }
     }
 }
+
+
+
+           
+  
+
+
+                  
