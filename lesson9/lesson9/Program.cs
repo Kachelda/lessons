89586035d01d8dddb05e34c0fd9fc623039a605b
kachelda <ni@syt.in.ua>
2017-07-21 22:57:03 +0300

    Papa!

--- lesson9/lesson9/Program.cs
+++ lesson9/lesson9/Program.cs
@@ -6,33 +6,94 @@ using System.Threading.Tasks;
 
 namespace lesson9
 {
+    enum Side
+    {
+        Left,
+        Right
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             List<List<int>> listfinal = new List<List<int>>();
             
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 4; i++)
             {
                 listfinal.Add(new List<int>());
                 
-                for (int j = 0; j < 3; j++)
+                for (int j = 0; j < 4; j++)
                 {
                     listfinal[i].Add(listfinal.Count + i + i + j);
                 }
-                
-                
-                
             }
 
             PrintTwoDimensional(listfinal);
             Console.WriteLine();
-            ChangeDiagonal2(listfinal);
+            Console.WriteLine();
+            ChangeDiagonalExtended(listfinal, Side.Right, 0);
             PrintTwoDimensional(listfinal);
             Console.WriteLine();
-            ChangeDiagonal2(listfinal);
-            PrintTwoDimensional(listfinal);
             Console.ReadLine();
+            
+        }
+
+        //общий метод по диагоналям
+        public static void ChangeDiagonalExtended(List<List<int>> list, Side S, int numd)
+        {
+            if (numd >= 0 && numd < list.Count )
+            {
+                switch (S)
+                {
+                    case Side.Left:
+                        ChangeDiagonalLeft(list, numd);
+                        break;
+                    case Side.Right:
+                        ChangeDiagonalRight(list, numd);
+                        break;
+                    default:
+                        Console.WriteLine("Некорректный ввод параметра <S>!");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод параметра <numd>!");
+            }
+        }
+        
+        // диагональ слева numd = i
+        public static void ChangeDiagonalLeft(List<List<int>> list, int numd)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                for (int j = 0; j < list.Count; j++)
+                {
+                    if (i == j + numd)
+                    {
+                        int k = list[i][j];
+                        list[i][j] = list[i + 1][ j + 1];
+                        list[i + 1][j + 1] = k;
+                    }
+                }
+            }
+        }
+
+        //диагональ справа numd = j
+        public static void ChangeDiagonalRight(List<List<int>> list, int numd)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = 0; j < list.Count - 1; j++)
+                {
+                    if (j == i + numd)
+                    {
+                        int k = list[i][j];
+                        list[i][j] = list[i + 1][j + 1];
+                        list[i + 1][j + 1] = k;
+                    }
+                }
+            }
         }
 
         //сдвиг диагонали вниз вправо
