1b4f666a8f8302725793ea7ae1483a15157d5295
kachelda <ni@syt.in.ua>
2017-07-24 22:07:45 +0300

    ky)

---
+++ lesson10/lesson10/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson10
+{
+    enum Side
+    {
+        Left,
+        Right,
+        Middle
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<int>> listfinal = new List<List<int>>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                listfinal.Add(new List<int>());
+
+                for (int j = 0; j < 4; j++)
+                {
+                    listfinal[i].Add(listfinal.Count + i + i + j);
+                }
+            }
+
+            PrintTwoDimensional(listfinal);
+            Console.WriteLine();
+            Console.WriteLine();
+            ChangeDiagonalExtended(listfinal, Side.Left, 1);
+            PrintTwoDimensional(listfinal);
+            Console.ReadLine();
+
+        }
+
+
+        //3 параметра входных
+        public static void ChangeDiagonalExtended(List<List<int>> list, Side S = Side.Middle, int numd = 0)
+        {
+            if (numd != list.Count - 1)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    for (int j = 0; j < list.Count; j++)
+                    {
+                        if (i == j + numd && i < list.Count - 1 && S == Side.Left || j == i + numd && j < list.Count - 1 && S == Side.Right || i == j && S == Side.Middle)
+                        {
+                            int k = list[i][j];
+                            list[i][j] = list[i + 1][j + 1];
+                            list[i + 1][j + 1] = k;
+                        }
+                    }
+                }
+            }
+        }
+        
+        public static void PrintTwoDimensional(List<List<int>> list)
+        {
+            Console.Write("{");
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                PrintOneDimensional(list[i]);
+
+                if (i + 1 < list.Count)
+                {
+                    Console.Write(",");
+                }
+            }
+            Console.Write("}");
+        }
+
+        public static void PrintOneDimensional(List<int> list, string separator = ",")
+        {
+            string result = "{" + string.Join(separator, list) + "}";
+            Console.Write(result);
+        }
+    }
+}
